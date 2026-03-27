82753aa3d09c5bea5b4a067d108456570fd2882b
Phantom <1224983495@qq.com>
2026-03-27 22:46:09 +0800

    logo视频

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,12 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
+[RequireComponent(typeof(VideoPlayer))]
 public class StartScene : MonoBehaviour
 {
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private string nextSceneName = "MainScene";
+
+    private bool _isLoading;
+
+    private void Reset()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    private void Awake()
+    {
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoFinished;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= OnVideoFinished;
+        }
+    }
+
+    private void Start()
+    {
+        if (videoPlayer != null && !videoPlayer.isPlaying)
+        {
+            videoPlayer.Play();
+        }
+    }
+
     public void OnLogoFinish()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadMainScene();
+    }
+
+    private void OnVideoFinished(VideoPlayer source)
+    {
+        long lastFrame = (long)source.frameCount - 1;
+        if (lastFrame >= 0)
+        {
+            source.frame = lastFrame;
+        }
+        source.Pause();
+        LoadMainScene();
+    }
+
+    private void LoadMainScene()
+    {
+        if (_isLoading)
+        {
+            return;
+        }
+
+        _isLoading = true;
+
+        if (string.IsNullOrWhiteSpace(nextSceneName))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+
+        SceneManager.LoadScene(nextSceneName);
     }
 }
